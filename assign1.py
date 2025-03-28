07f448d5eaa8340826d40e898c2a065f2ff1a1d1
rawanhotak <1091840@pdsb.net>
2025-03-28 13:01:39 -0400

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -1,1 +1,15 @@
-print("Hello")
+print("Welcome to the even and odd detector!")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if a % 2 == 0:
+    print(f"{a} is even.")
+else:
+    print(f"{a} is odd.")
+if b % 2 == 0:
+    print(f"{b} is even.")
+else:
+    print(f"{b} is odd.")
+if (a % 2 == 0) or (b % 2 == 0):
+    print("The product will be even.")
+else:
+    print("The product will be odd.")                
