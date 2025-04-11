7792932da4be317be4c30d4873ec43c3df9bf454
rawanhotak <1091840@pdsb.net>
2025-04-11 10:39:41 -0400

    Update part3.py

--- part3.py
+++ part3.py
@@ -10,7 +10,7 @@ pennies — the remaining cents that are less than 5, representing pennies
 # This function takes a value in cents and prints the number of quarters, dimes, nickels, and pennies needed to make that amount
 """
 # Check if the cents amount is greater than 100 and calculate the remaining cents after removing whole dollars
-cent5s=int(input("please enter the value of cents:"))
+cents=int(input("please enter the value of cents:"))
 if cents > 100:
      cents= cents%100# Get the remainder after dividing by 100 (removes the dollar part)
      # Calculate the number of quarters (25 cents) in the remaining amount
@@ -19,3 +19,13 @@ cents %=25 # Update the remaining cents after removing the quarters
 # Calculate the number of dimes (10 cents) in the remaining amount
 dimes = cents // 10 # Integer division to find how many dimes fit in the remaining cents
 cents %=10# Update the remaining cents after removing the dime
+#caluclate nickels
+nickels= cents//5
+cents%=5
+#remaining are pennies
+pennies=cents 
+#print results
+print(f"Quarters:{quarters}")
+print(f"Dimes:{dimes}")
+print(f"Nickels:{nickels}")
+print(f"Pennies:{pennies}")
