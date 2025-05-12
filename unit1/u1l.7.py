38cc0947b2dddf910f5e50e4b980ad96bcd863f6
rawanhotak <1091840@pdsb.net>
2025-05-12 13:00:03 -0400

    Create u1l.7.py

---
+++ unit1/u1l.7.py
@@ -0,0 +1,15 @@
+#Example run 1: fails for x:
+Please input a whole number betwqeen 1 and 20: 50
+50 is outside of range. Cannot continue.
+#Example run 2: fails for y:
+Please input a whole number betwqeen 1 and 20: 5
+Please input another whole number betwqeen 1 and 20: 500
+500 is outside of range. Cannot continue.
+#Example run 3: y is a factor of x
+Please input a whole number betwqeen 1 and 20: 16
+Please input another whole number betwqeen 1 and 20: 8
+Yes! 8 is a factor of 16!
+Example run 4: y is not a factor of x:
+Please input a whole number betwqeen 1 and 20: 16
+Please input another whole number betwqeen 1 and 20: 7
+7 is not a factor of 16.
