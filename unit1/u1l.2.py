1668c765aee4284b41daf63c9cdd7df3c911b200
rawanhotak <1091840@pdsb.net>
2025-05-12 12:19:45 -0400

    Update and rename uniu1less2 to u1l.2.py

---
+++ unit1/u1l.2.py
@@ -0,0 +1,9 @@
+q = 18
+q = q - 1 # using subtraction
+q -= 1 # does the same thing
+# prints out "The value is now: 16"
+print("The value is now: ", q) 
+q = q / 2 # using division
+q /= 2 # same thing
+# prints out "The value is now: 4.0"
+print("The value is now: ", q) 
