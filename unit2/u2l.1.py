de4750c4a244200bdc9116829e21a0c0b72e25dc
rawanhotak <1091840@pdsb.net>
2025-05-12 13:01:33 -0400

    Create u2l.1.py

---
+++ unit2/u2l.1.py
@@ -0,0 +1,11 @@
+print("Make a choice from the following menu: ")
+print("A: apples")
+print("B: bananas")
+print("C: cherries")
+ch = input("Your choice: ")
+if (ch == "A"):
+    print("I prefer apples")
+elif (ch == "B"):
+    print("I prefer bananas")
+else:
+    print("I prefer cherries")
