092ff4585be89b133c282b5a9ce9155dcf7da1f9
rawanhotak <1091840@pdsb.net>
2025-05-27 13:32:17 -0400

    Create u3l.4.py

---
+++ unit3/u3l.4.py
@@ -0,0 +1,9 @@
+ar2 = [[3, 4, 1, 2, 6],
+      [9, 2, 3, 7, 5],
+      [4, 2, 1, 0, 3]]
+for i in range(len(ar2)):
+  ar3 = ar2[i]
+  for j in range(len(ar3)):
+      print(ar3[j], end=" ")
+  print() # PREDICT A
+print(ar2) # PREDICT B
