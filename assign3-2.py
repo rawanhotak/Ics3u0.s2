67645b97a1084a25d5e3a7a5d7b8eccc6a88564f
rawanhotak <1091840@pdsb.net>
2025-05-27 14:07:35 -0400

    Create assign3-2.py

---
+++ assign3-2.py
@@ -0,0 +1,25 @@
+# List of words to check
+word_list = ["racecar", "noon"
+', "desk", "madam",
+"level"
+"store"
+', "rotor", "civic"
+', "apple"
+"refer"]
+# Function to check if a word is a palindrome
+def is_palindrome (word):
+length = len (word)
+for i in range(length // 2) :
+if wordlil != wordilength - 1 - 11:
+return False
+return True
+# Loop through the list and print whether each
+word is a palindrome
+print ("Palindrome program!") for word in word_list:
+if is_ palindrome (word) :
+print(f"{word} is
+a
+palindrome")
+else:
+print(f"{word} is not a palindrome")
+print ("'Goodbye!")
