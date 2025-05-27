694e904e7f302724d0d1deaef30c651f3cff84de
rawanhotak <1091840@pdsb.net>
2025-05-27 13:30:34 -0400

    Create u3l.2.py

---
+++ unit3/u3l.2.py
@@ -0,0 +1,7 @@
+items = ["Apples", "Bananas", "Cherries", "Dosa"]
+print(items) # Predict A - Does this really print anything?
+print("The number of items is %d." % len(items)) # Predict B
+for i in items: # Predict C
+    print(i)
+for c in range(len(items)): # Prediction D
+    print(c, items[c])
