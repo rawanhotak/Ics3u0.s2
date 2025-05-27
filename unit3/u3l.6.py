a0242f943d3e7d30b1e37986e151a9e1fa6d105d
rawanhotak <1091840@pdsb.net>
2025-05-27 13:34:39 -0400

    Create u3l.6.py

---
+++ unit3/u3l.6.py
@@ -0,0 +1,6 @@
+get the number N
+find the square root of N; throw away the decimal(floor function)
+  counting j from i to N
+     if n % j == 0 then j is a factor
+     add j to your list of factors
+return the list of factors
