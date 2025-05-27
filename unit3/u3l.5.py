1c73b618ec8ef87e6f692fb4fba5c33de1f16d24
rawanhotak <1091840@pdsb.net>
2025-05-27 13:32:53 -0400

    Create u3l.5.py

---
+++ unit3/u3l.5.py
@@ -0,0 +1,6 @@
+Enter a loop
+   go to the next array member (N)
+   Compare N to highest
+   If highest < N 
+      highest = N
+After the loop exits, you should have the highest array member in the variable "highest"
