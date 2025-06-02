96d597abd714e02d9e6b4a4dc887324e110054f8
rawanhotak <1091840@pdsb.net>
2025-06-02 12:33:38 -0400

    Update assign3-2.py

--- assign3-2.py
+++ assign3-2.py
@@ -1,35 +1,25 @@
 """
-rawan abdelbasit
-1091840
 ICS3U
 date:28/5/2025
+word_list: stores the list of words to check for palindrome
 virables:word_list: stores the list of words to check for palindrome
 word: used in the loop to access each word from the list
 i: used as an index in the loop to compare letters
 Length: stores the number of characters in each word to help compare from both ends
 """
 # List of words to check
-word_list = ["racecar", "noon"
-', "desk", "madam",
-"level"
-"store"
-', "rotor", "civic"
-', "apple"
-"refer"]
+word_list = ["racecar", "noon", "desk", "madam","level","store","rotor","civic","apple","refer"]
 # Function to check if a word is a palindrome
 def is_palindrome (word):
-length = len (word)
+    length = len (word)
 for i in range(length // 2) :
-if wordlil != wordilength - 1 - 11:
-return False
-return True
+    if wordlil != word[length - 1 - i]:
+    return False
+            return True
 # Loop through the list and print whether each
-word is a palindrome
-print ("Palindrome program!") for word in word_list:
-if is_ palindrome (word) :
-print(f"{word} is
-a
-palindrome")
+print ("Palindrome program!")
+for word in word_list:
+    if is_palindrome(word):
+      print(f"{word} is a palindrome")
 else:
-print(f"{word} is not a palindrome")
-print ("'Goodbye!")
+      print(f"{word} is not a palindrome")
